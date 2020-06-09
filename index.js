1cb94e08e7126c40e90c413c27126dce66c4ec25
Glenn Jensen <g.e.jensen@gmail.com>
2020-06-09 17:21:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+
+// function 1 - Uppercase
+function shout(string) {
+  return string.toUpperCase()
+}
+
+// function 2 - lowercase
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+// function 3 - log shout
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+// function 4 - log whisper
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+// last one Grandma
+function SayHiToGrandma(string) {
+
+if
+
+
+/* switch(string) {
+  case string.toUpperCase() === string :
+   return "YES INDEED!";
+   break;
+  case string.toLowerCase() === string :
+   return "I can/'t hear you!";
+   break;
+  case string === "I love you, Grandma." :
+   return "I love you, too.";
+ }*/
+}
